4ff62c00c132f8084958be47c1c7e43aaac0008c
dominik-pm <dominik25664@gmail.com>
2021-10-05 17:33:10 +0200

    aesthetic update

--- PositionAlgo/index.js
+++ PositionAlgo/index.js
@@ -1,14 +1,13 @@
 require('dotenv').config({ path: './variables.env' })
 
 const express = require('express')
-const Joi = require('joi')
 const authenticate = require('./middleware/authenticate')
-const {logTime, logErr} = require('./middleware/logger')
+const { logTime, logErr } = require('./middleware/logger')
 const { updatePosition } = require('./middleware/positionupdate')
 const app = express()
 const port = process.argv[2] || 8088
 
-app.use(express.json()) // parse 
+app.use(express.json()); 
 app.use(logTime);
 
 app.get('/positionupdate', authenticate, updatePosition, (req, res) => {
@@ -18,17 +17,12 @@ app.get('/positionupdate', authenticate, updatePosition, (req, res) => {
         console.error(`newStopLoss: ${newStopLoss}`);
         console.error(`newTakeProfit: ${newTakeProfit}`);
         throw('internal error: no newStopLoss or newTakeProfit')
-        // console.log('internal error: no newStopLoss or newTakeProfit');
-        // return res.status(541).send({message: `internal server error!`})
     }
 
     let resObj = {
         stopLoss: newStopLoss,
         takeProfit: newTakeProfit
     }
-
-    // console.log(`responding with:`)
-    // console.log(resObj)
 
     res.status(200).send(resObj)
 
