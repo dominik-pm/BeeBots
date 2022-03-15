a582c52b087f4d434508aaf7c7dae874f9177cc7
Dominik Pömmer <dominik25664@gmail.com>
2022-03-15 16:18:03 +0100

    removed clg

--- BotHandler/src/api/BuyAlgo.ts
+++ BotHandler/src/api/BuyAlgo.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 import { BuyAlgoRequest, BuyAlgoResponse } from '../@types/api/BuyAlgo'
 import { formatAxiosError, getAxiosRequestConfig } from './Api'
 
-export const BUYALGO_URL: string = 'http://buyalgo.azurewebsites.net' // 'http://localhost:8087'
+// export const BUYALGO_URL: string = 'http://buyalgo.azurewebsites.net' // 'http://localhost:8087'
+export const BUYALGO_URL: string = 'http://localhost:8087'
 
 // --> BuyAlgo
 export async function getTradeCall(token: string, data: BuyAlgoRequest, buyAlgo: number): Promise<BuyAlgoResponse> {
     return new Promise((resolve, reject) => {
 
-        console.log('buyalgo req')
         axios.defaults.headers.common['x-algo'] = buyAlgo.toString()
         axios.get(`${BUYALGO_URL}/tradecall`, getAxiosRequestConfig(token, data))
         .then(res => {
